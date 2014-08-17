aa1c6c1de09da68db7e3f8294b199256e532e45e
Mario Döring <deluna.yy@gmail.com>
2014-08-17 22:12:28 +0200

    Phpunit cleanups

--- CCF/earth/bundles/pages/database/pages/redirect_1408129236.sql
+++
@@ -1,7 +0,0 @@
-# ---> up
-
-ALTER TABLE  `earth_pages` ADD  `redirect` VARCHAR( 255 ) NOT NULL AFTER  `controller` ;
-
-# ---> down
-
-ALTER TABLE `earth_pages` DROP `redirect`;
--- CCF/earth/bundles/pages/database/pages/type_1408203074.sql
+++
@@ -1,7 +0,0 @@
-# ---> up
-
-ALTER TABLE  `earth_pages` ADD `type` tinyint(1) NOT NULL AFTER  `id` ;
-
-# ---> down
-
-ALTER TABLE `earth_pages` DROP `type`;
